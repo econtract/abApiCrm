f5cb5d298c2605bddcf572832e82ffb69ab20b00
Imran <imran@zeropoint.it>
2017-09-15 23:42:59 +0500

    Added AJAX save portions of orders code.

--- js/orders.js
+++ js/orders.js
@@ -26,15 +26,15 @@ jQuery(document).ready(function ($) {
                 if (jsonRes.available == false) {
                     html = '<div class="alert alert-danger">' +
                         '<p>' + jsonRes.msg + '</p>' +
-                        '<a href="#change-zip" onclick="jQuery(\'#checkAvailabilityForm\').toggle();jQuery(\'#ModalCheckAvailability .desc\').toggle();jQuery(\'#ModalCheckAvailability .content-error\').toggle();jQuery(\'#ModalCheckAvailability .contact-lnk\').toggle();jQuery(\'#ModalCheckAvailability  .alert.alert-danger\').toggle();">('+site_obj.change_zip_trans+')</a>' +
+                        '<a href="#change-zip" onclick="jQuery(\'#checkAvailabilityForm\').toggle();jQuery(\'#ModalCheckAvailability .desc\').toggle();jQuery(\'#ModalCheckAvailability .content-error\').toggle();jQuery(\'#ModalCheckAvailability .contact-lnk\').toggle();jQuery(\'#ModalCheckAvailability  .alert.alert-danger\').toggle();">(' + site_obj.change_zip_trans + ')</a>' +
                         '</div>';
                     //html += '<a href="'+site_obj.contact_uri+'" class="modal-btm-link contact-lnk"><i class="fa fa-angle-right"></i> '+site_obj.contact_trans+'</a>';
                     html += jsonRes.html;//append any HTML returned by AJAX response this includes form and submit button
                 }
-                else if(jsonRes.available == true) {
+                else if (jsonRes.available == true) {
                     html = '<div class="alert alert-success">' +
                         '<p>' + jsonRes.msg + '</p>' +
-                        '<a href="#change-zip" onclick="jQuery(\'#ModalCheckAvailability form\').toggle();jQuery(\'#ModalCheckAvailability .desc\').toggle();jQuery(\'#ModalCheckAvailability .modal-list\').toggle();jQuery(\'#ModalCheckAvailability  .alert.alert-success\').toggle();">('+site_obj.change_zip_trans+')</a>' +
+                        '<a href="#change-zip" onclick="jQuery(\'#ModalCheckAvailability form\').toggle();jQuery(\'#ModalCheckAvailability .desc\').toggle();jQuery(\'#ModalCheckAvailability .modal-list\').toggle();jQuery(\'#ModalCheckAvailability  .alert.alert-success\').toggle();">(' + site_obj.change_zip_trans + ')</a>' +
                         '</div>';
                     html += jsonRes.html;//append any HTML returned by AJAX response this includes form and submit button
 
@@ -50,9 +50,31 @@ jQuery(document).ready(function ($) {
             }
             $('#ModalCheckAvailability .modal-body').append(html);
         });
-
     });
 
+    $("#select_provider").on('change', function () {
+        var changedProviderObj = $(this).find('option:selected');
+        var changedProviderTxt = $(changedProviderObj).text();
+        var changedProviderVal = $(changedProviderObj).val();
 
+        console.log(changedProviderObj, changedProviderTxt, changedProviderVal);
+        $('span.selected_supplier').text(changedProviderTxt);
+    });
+
+    //Order steps, for the forms that are without array called as simple forms,
+    //this means that the input variables are not this way e.g. form_input[], or form_input['order'][] etc
+    $(".order-simple-form .btn-nxt a").on('click', function(e) {
+        //e.preventDefault();
+        var inputForm = $(this).parents('form.order-simple-form');
+        var formInputs = $(inputForm).serialize()+'&action=saveSimpleOrder&'+$('#orderCommon').serialize();
+        console.log(formInputs);
+        /*var data = {
+            'action': 'saveSimpleOrder',
+        };*/
+        var data = formInputs;
+        $.post(site_obj.ajax_url, data, function (response) {
+            console.log(response);
+        });
+    });
 });
 
