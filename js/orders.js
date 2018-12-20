1ce515594a72ef027927632a045d0b3f4559855e
dhakim <danish.hakim@gmail.com>
2018-12-20 15:48:36 +0500

    internal mode condition for ids fields

--- js/orders.js
+++ js/orders.js
@@ -558,26 +558,27 @@ jQuery(document).ready(function ($) {
 
     $("input[name=client_nationality]").on('change', function(e) {
         e.stopPropagation();
-        var idcardEl = $('#client_idnr'),
-        nat = $(this).val();
-        var natParent = $('#client_idnr').parents('div.form-group');
-        var prevIdnrVal = $('#client_idnr').val();
-        idcardEl.val('');
+        if($('#client_idnr').length > 0) {
+            var idcardEl = $('#client_idnr'),
+                nat = $(this).val();
+            var natParent = $('#client_idnr').parents('div.form-group');
+            var prevIdnrVal = $('#client_idnr').val();
+            idcardEl.val('');
 
-        if(nat == 'BE') {
-            idcardEl.remove();
-            natParent.prepend('<input type="text" class="form-control telecom-order4-idcard" ' +
-                'id="client_idnr" name="client_idnr" placeholder="591-0123456-78" data-idcard=""' +
-                'value="' + prevIdnrVal + '" data-error="' + site_obj.idcard_error + '" required>');
-            idcardEl.mask("000-0000000-00");
-        } else {
-            idcardEl.remove();//Removing because unmask doesn't work well, as all of unmasking methods don't work reliably
-            natParent.prepend('<input type="text" class="form-control" ' +
-                'id="client_idnr" name="client_idnr" placeholder="" ' +
-                'value="" data-error="' + site_obj.idcard_error + '" required>');
-            idcardEl.unmask();
+            if (nat == 'BE') {
+                idcardEl.remove();
+                natParent.prepend('<input type="text" class="form-control telecom-order4-idcard" ' +
+                    'id="client_idnr" name="client_idnr" placeholder="591-0123456-78" data-idcard=""' +
+                    'value="' + prevIdnrVal + '" data-error="' + site_obj.idcard_error + '" required>');
+                idcardEl.mask("000-0000000-00");
+            } else {
+                idcardEl.remove();//Removing because unmask doesn't work well, as all of unmasking methods don't work reliably
+                natParent.prepend('<input type="text" class="form-control" ' +
+                    'id="client_idnr" name="client_idnr" placeholder="" ' +
+                    'value="" data-error="' + site_obj.idcard_error + '" required>');
+                idcardEl.unmask();
+            }
         }
-
         $(this).parents('form').validator('destroy');
         $(this).parents('form').validator('update');
     });
