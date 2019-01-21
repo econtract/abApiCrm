921a44faa7fe76a0316a2c1364ac206e5430e75c
Tauqeer Abbas <tauqeer.abbas@zeropoint.it>
2019-01-21 17:25:02 +0500

    multiple zipcode fields issue fixed

--- abApiCrm.php
+++ abApiCrm.php
@@ -93,7 +93,7 @@ class abApiCrm {
 				'jquery',
 				'jquery-bootstrap-typeahead',
 				'aanbieder_default_script'
-			), '2.2.2', true );
+			), '2.2.3', true );
 
 			wp_localize_script( 'crm-script-orders', 'site_obj',
 				array(
